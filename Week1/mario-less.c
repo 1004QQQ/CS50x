3c0a11be9efbb91a6b6a551bb7e9bfcfca91681c
Jiayi Qiu <jiayiqw1004@gmail.com>
2025-04-10 21:09:35 +0100

    Create mario-less.c

---
+++ Week1/mario-less.c
@@ -0,0 +1,32 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void printrow(int bricks, int height);
+
+int main(void)
+{
+    int height;
+    do
+    {
+        // prompt user for height
+        height = get_int("Height: ");
+    }
+    while (1 > height || height > 8);
+
+    for (int i = 0; i < height; i++)
+        printrow(i + 1, height);
+}
+
+// function that prints a row
+void printrow(int bricks, int height)
+{
+    for (int i = 0; i < height - bricks; i++)
+    {
+        printf(" ");
+    }
+    for (int i = 0; i < bricks; i++)
+    {
+        printf("#");
+    }
+    printf("\n");
+}
